ed966c48c7365ee0b99ec0e9b3435ad856a663aa
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2021-03-05 01:46:45 +0800

    remove duplicates done

---
+++ interview/remove-duplicates-vector(medium).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using std::cout;
+using std::endl;
+
+int remove_duplicates(std::vector<int>& numbers)
+{
+    int length = numbers.size();
+    int counter = 0;
+
+    if (length == 0) return 0;
+    else if (length == 1) return 1;
+    else{
+        for(int i = 0; i < length; ++i){
+            if(numbers[i] != numbers[i+1]) 
+                counter++;
+        }    
+    }
+    return counter;
+}
+
+int main(int argc, char const *argv[])
+{
+    std::vector<int> numbers{1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6};
+
+    cout << "Original size before removal: " << numbers.size() << endl;
+
+    int result = remove_duplicates(numbers);
+
+    cout << result << endl;
+
+    return 0;
+}
